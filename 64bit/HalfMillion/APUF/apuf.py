75f6ed83744649768d5af71d3d61ef094f8e426c
Sherlock <Sherlock-Hou@github.com>
2020-03-02 11:00:23 +0800

    Update apuf.py

--- 64bit/HalfMillion/APUF/apuf.py
+++ 64bit/HalfMillion/APUF/apuf.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn import cross_validation
+# from sklearn import cross_validation
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense,Dropout
